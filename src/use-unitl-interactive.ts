cc200fedef1c8b943730125cc61a87b17520761e
Hank Lin <jyunhanlin@gmail.com>
2023-04-02 17:00:43 +0800

    feat: update useUntilInteractive

--- src/use-unitl-interactive.ts
+++ src/use-unitl-interactive.ts
@@ -1,10 +1,12 @@
-import { useEffect } from 'react';
+import { useRef } from 'react';
 
 import type { Options } from './types';
 import { untilInteractive } from './until-interactive';
 
-export const useUntilInteractive = (options: Options, deps: any[]) => {
-  useEffect(() => {
-    untilInteractive(options);
-  }, deps);
+export const useUntilInteractive = (options: Options) => {
+  const willMount = useRef(true);
+
+  if (willMount.current) untilInteractive(options);
+
+  willMount.current = false;
 };
